9b445c38865075f23df7c2c5885d286be1aaf41b
evelyngutierrez095 <evelyn.gutierrez095@pascualbravo.edu.co>
2026-02-19 22:22:17 -0500

    Add files via upload

---
+++ TrabajoClase/formularios/Principal.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TrabajoClase
+{
+    partial class Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            principalToolStripMenuItem = new ToolStripMenuItem();
+            tablasToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            productosToolStripMenuItem = new ToolStripMenuItem();
+            categoriaToolStripMenuItem = new ToolStripMenuItem();
+            facturacionToolStripMenuItem = new ToolStripMenuItem();
+            facturasToolStripMenuItem = new ToolStripMenuItem();
+            informesToolStripMenuItem = new ToolStripMenuItem();
+            seguridadToolStripMenuItem = new ToolStripMenuItem();
+            empleadoToolStripMenuItem = new ToolStripMenuItem();
+            rolesToolStripMenuItem = new ToolStripMenuItem();
+            seguridadToolStripMenuItem1 = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem1 = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { principalToolStripMenuItem, tablasToolStripMenuItem, facturacionToolStripMenuItem, seguridadToolStripMenuItem, ayudaToolStripMenuItem, salirToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // principalToolStripMenuItem
+            // 
+            principalToolStripMenuItem.Name = "principalToolStripMenuItem";
+            principalToolStripMenuItem.Size = new Size(65, 20);
+            principalToolStripMenuItem.Text = "Principal";
+            principalToolStripMenuItem.Click += principalToolStripMenuItem_Click;
+            // 
+            // tablasToolStripMenuItem
+            // 
+            tablasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clientesToolStripMenuItem, productosToolStripMenuItem, categoriaToolStripMenuItem });
+            tablasToolStripMenuItem.Name = "tablasToolStripMenuItem";
+            tablasToolStripMenuItem.Size = new Size(52, 20);
+            tablasToolStripMenuItem.Text = "Tablas";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(128, 22);
+            clientesToolStripMenuItem.Text = "Clientes";
+            clientesToolStripMenuItem.Click += clientesToolStripMenuItem_Click;
+            // 
+            // productosToolStripMenuItem
+            // 
+            productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+            productosToolStripMenuItem.Size = new Size(128, 22);
+            productosToolStripMenuItem.Text = "Productos";
+            productosToolStripMenuItem.Click += productosToolStripMenuItem_Click;
+            // 
+            // categoriaToolStripMenuItem
+            // 
+            categoriaToolStripMenuItem.Name = "categoriaToolStripMenuItem";
+            categoriaToolStripMenuItem.Size = new Size(128, 22);
+            categoriaToolStripMenuItem.Text = "Categoria";
+            categoriaToolStripMenuItem.Click += categoriaToolStripMenuItem_Click;
+            // 
+            // facturacionToolStripMenuItem
+            // 
+            facturacionToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { facturasToolStripMenuItem, informesToolStripMenuItem });
+            facturacionToolStripMenuItem.Name = "facturacionToolStripMenuItem";
+            facturacionToolStripMenuItem.Size = new Size(81, 20);
+            facturacionToolStripMenuItem.Text = "Facturacion";
+            // 
+            // facturasToolStripMenuItem
+            // 
+            facturasToolStripMenuItem.Name = "facturasToolStripMenuItem";
+            facturasToolStripMenuItem.Size = new Size(121, 22);
+            facturasToolStripMenuItem.Text = "facturas";
+            facturasToolStripMenuItem.Click += facturasToolStripMenuItem_Click;
+            // 
+            // informesToolStripMenuItem
+            // 
+            informesToolStripMenuItem.Name = "informesToolStripMenuItem";
+            informesToolStripMenuItem.Size = new Size(121, 22);
+            informesToolStripMenuItem.Text = "informes";
+            informesToolStripMenuItem.Click += informesToolStripMenuItem_Click;
+            // 
+            // seguridadToolStripMenuItem
+            // 
+            seguridadToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { empleadoToolStripMenuItem, rolesToolStripMenuItem, seguridadToolStripMenuItem1 });
+            seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
+            seguridadToolStripMenuItem.Size = new Size(72, 20);
+            seguridadToolStripMenuItem.Text = "Seguridad";
+            // 
+            // empleadoToolStripMenuItem
+            // 
+            empleadoToolStripMenuItem.Name = "empleadoToolStripMenuItem";
+            empleadoToolStripMenuItem.Size = new Size(127, 22);
+            empleadoToolStripMenuItem.Text = "Empleado";
+            empleadoToolStripMenuItem.Click += empleadoToolStripMenuItem_Click;
+            // 
+            // rolesToolStripMenuItem
+            // 
+            rolesToolStripMenuItem.Name = "rolesToolStripMenuItem";
+            rolesToolStripMenuItem.Size = new Size(127, 22);
+            rolesToolStripMenuItem.Text = "roles";
+            rolesToolStripMenuItem.Click += rolesToolStripMenuItem_Click;
+            // 
+            // seguridadToolStripMenuItem1
+            // 
+            seguridadToolStripMenuItem1.Name = "seguridadToolStripMenuItem1";
+            seguridadToolStripMenuItem1.Size = new Size(127, 22);
+            seguridadToolStripMenuItem1.Text = "seguridad";
+            seguridadToolStripMenuItem1.Click += seguridadToolStripMenuItem1_Click;
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ayudaToolStripMenuItem1, acercaDeToolStripMenuItem });
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(53, 20);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // ayudaToolStripMenuItem1
+            // 
+            ayudaToolStripMenuItem1.Name = "ayudaToolStripMenuItem1";
+            ayudaToolStripMenuItem1.Size = new Size(124, 22);
+            ayudaToolStripMenuItem1.Text = "ayuda";
+            ayudaToolStripMenuItem1.Click += ayudaToolStripMenuItem1_Click;
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(124, 22);
+            acercaDeToolStripMenuItem.Text = "acerca de";
+            acercaDeToolStripMenuItem.Click += acercaDeToolStripMenuItem_Click;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(41, 20);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "Principal";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem principalToolStripMenuItem;
+        private ToolStripMenuItem tablasToolStripMenuItem;
+        private ToolStripMenuItem facturacionToolStripMenuItem;
+        private ToolStripMenuItem facturasToolStripMenuItem;
+        private ToolStripMenuItem informesToolStripMenuItem;
+        private ToolStripMenuItem seguridadToolStripMenuItem;
+        private ToolStripMenuItem empleadoToolStripMenuItem;
+        private ToolStripMenuItem rolesToolStripMenuItem;
+        private ToolStripMenuItem seguridadToolStripMenuItem1;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem1;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem productosToolStripMenuItem;
+        private ToolStripMenuItem categoriaToolStripMenuItem;
+    }
+}
---
+++ TrabajoClase/formularios/login.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TrabajoClase
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Usuario = new Label();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            label2 = new Label();
+            btnIngreso = new Button();
+            pictureBox1 = new PictureBox();
+            btnSalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // Usuario
+            // 
+            Usuario.AutoSize = true;
+            Usuario.Location = new Point(269, 165);
+            Usuario.Name = "Usuario";
+            Usuario.Size = new Size(47, 15);
+            Usuario.TabIndex = 0;
+            Usuario.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(342, 165);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(433, 23);
+            txtUsuario.TabIndex = 1;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(342, 204);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(433, 23);
+            txtContraseña.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(269, 204);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Contraseña";
+            // 
+            // btnIngreso
+            // 
+            btnIngreso.Location = new Point(297, 263);
+            btnIngreso.Name = "btnIngreso";
+            btnIngreso.Size = new Size(220, 57);
+            btnIngreso.TabIndex = 4;
+            btnIngreso.Text = "Ingresar";
+            btnIngreso.UseVisualStyleBackColor = true;
+            btnIngreso.Click += btnIngreso_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(251, 450);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(523, 263);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(220, 57);
+            btnSalir.TabIndex = 6;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnIngreso);
+            Controls.Add(txtContraseña);
+            Controls.Add(label2);
+            Controls.Add(txtUsuario);
+            Controls.Add(Usuario);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "login";
+            StartPosition = FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Usuario;
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private Label label2;
+        private Button btnIngreso;
+        private PictureBox pictureBox1;
+        private Button btnSalir;
+    }
+}
